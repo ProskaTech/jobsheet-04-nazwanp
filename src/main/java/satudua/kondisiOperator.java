67a65dc0b7501e265d6e1f1286c47a6b7c32a6a3
Nazwa Nurfadhilla Phitri <112637218+nazwanp@users.noreply.github.com>
2022-09-15 09:34:05 +0700

    Add files via upload

---
+++ src/main/java/satudua/kondisiOperator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package satudua;
+
+/**
+ *
+ * @author acer
+ */
+public class kondisiOperator {
+    public static void main(String[] args) {
+    String status = "";
+    int grade = 50;
+    
+    //mendapatkan status pelajar
+    status = (grade >= 60) ? "Passed" : "Fail";
+    
+    //print status
+    System.out.println(status);
+    }
+}
