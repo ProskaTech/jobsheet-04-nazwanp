3f30d992be54c5a7c1a89c577f91c94a87c1aa44
Nazwa Nurfadhilla Phitri <112637218+nazwanp@users.noreply.github.com>
2022-09-15 09:52:21 +0700

    Add files via upload

---
+++ src/main/java/satudua/TestNOT.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package satudua;
+
+/**
+ *
+ * Created by Nazwa Nurfadhilla Phitri
+ */
+public class TestNOT {
+    public static void main(String[] args) {
+        boolean va11 = true;
+        boolean va12 = false;
+        
+        System.out.println(!va11);
+        System.out.println(!va12);
+    }
+}
